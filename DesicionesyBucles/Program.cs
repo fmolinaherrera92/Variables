80a953e891a8cccafc76788b8dbc8cbef4c4d9c8
Ing, Molina <69854303+fmolinaherrera92@users.noreply.github.com>
2022-09-27 18:07:10 -0400

    OperadorAnduOR

--- DesicionesyBucles/Program.cs
+++ DesicionesyBucles/Program.cs
@@ -6,7 +6,48 @@ namespace DesicionesyBucles
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //ComparaciondeUtilizandoBooleano();
+            OperadorAnduOR();
         }
+
+        public static void OperadorAnduOR() 
+        {
+            var cantidad = 6;
+
+            var estaEntre5y10 = cantidad > 5 && cantidad < 10; // true
+            Console.WriteLine($"Esta el {cantidad} entre 5 y 10? {estaEntre5y10}");
+
+        }
+
+        public static void ComparaciondeUtilizandoBooleano() 
+        {
+            var cantidad1 = 5;
+            var cantidad2 = 5;
+            //Comparacion de utilizando booleano
+            var cantidad1IgualCantidad2 = cantidad1 == cantidad2; // true
+
+            var cantidad1esDiferenteACantidad2 = cantidad1 != cantidad2; // false
+
+            Console.WriteLine($"son {cantidad1} y {cantidad2} iguales?{cantidad1IgualCantidad2}");
+            Console.WriteLine($"son {cantidad1} y {cantidad2} es diferente? {cantidad1esDiferenteACantidad2}");
+
+            var cantidad1MenorQue10 = cantidad1 < 10; // true
+            var cantidad1MayorQue10 = cantidad1 > 10; // false
+
+            var cantidad1MenorOIgualQue10 = cantidad1 <= 10; // true
+            var cantidad1MayorOIgualQue10 = cantidad1 >= 10; // false
+
+            string nombre1 = null;
+            var nombre2 = "Francisco";
+
+            var sonLosNombresIguales = nombre1 == nombre2; // true
+
+            var esNombre1Nulo = nombre1 is null; // false
+            var esNombre1NoNulo = nombre1 is not null; // true
+            Console.WriteLine("Es nombre 1  igual a nulo?" + esNombre1Nulo);
+            Console.WriteLine("Es nombre 1  diferente de nulo?" + esNombre1NoNulo);
+        }
+
     }
-}
+    }
+
